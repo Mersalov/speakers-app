da6c76abed9aec61a1f540e43a34698ebd04d7e7
Mersalov <dmitrii.mersalov@gmail.com>
2024-08-16 03:05:45 +0300

    chore(modules): scaffold core, shared, infrastructure, and features/speakers modules
    Set up the initial module structure for the application by scaffolding the core, shared,
    infrastructure, and features/speakers modules. No components or further development have been added
    at this stage

---
+++ src/app/features/speakers/speakers.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpeakersRoutingModule } from './speakers-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SpeakersRoutingModule
+  ]
+})
+export class SpeakersModule { }
